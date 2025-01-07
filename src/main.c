722ac7583d19f7773f35d89950f71b2ca8883f74
kureta <skureta@gmail.com>
2025-01-07 23:07:08 +0300

    print ip address

--- src/main.c
+++ src/main.c
@@ -27,6 +27,15 @@ static err_t accept_callback(void *arg, struct tcp_pcb *newpcb, err_t err) {
   return ERR_OK;
 }
 
+static void netif_status_callback(struct netif *netif) {
+  if (netif_is_up(netif) && !ip4_addr_isany_val(*netif_ip4_addr(netif))) {
+    printf("Network is up\n");
+    printf("IP Address: %s\n", ip4addr_ntoa(netif_ip4_addr(netif)));
+  } else {
+    printf("Network is down\n");
+  }
+}
+
 int main() {
   stdio_init_all();
   sleep_ms(1000);
@@ -37,6 +46,9 @@ int main() {
     return 1;
   }
   cyw43_arch_enable_sta_mode();
+
+  struct netif *netif = &cyw43_state.netif[CYW43_ITF_STA];
+  netif_set_status_callback(netif, netif_status_callback);
 
   printf("Connecting to Wi-Fi...\n");
   if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASS,
