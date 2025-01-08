5cec8c7a3ad972bf1eb98b75615cbf3a9b8ed1ae
kureta <skureta@gmail.com>
2025-01-09 00:15:01 +0300

    move to cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,10 @@
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h" // IWYU pragma: keep
 
-#include "lwip/pbuf.h"
-#include "lwip/tcp.h"
+#include "PicoOSC.hpp"
 
-#define PORT 8080
-
-static err_t recv_callback(void *arg, struct tcp_pcb *tpcb, struct pbuf *p,
-                           err_t err) {
-  if (p == NULL) {
-    tcp_close(tpcb);
-    return ERR_OK;
-  }
-
-  tcp_recved(tpcb, p->tot_len);
-  tcp_write(tpcb, p->payload, p->len, TCP_WRITE_FLAG_COPY);
-  printf("Received: %.*s\n", p->len, (char *)p->payload);
-  pbuf_free(p);
-
-  return ERR_OK;
-}
-
-static err_t accept_callback(void *arg, struct tcp_pcb *newpcb, err_t err) {
-  tcp_recv(newpcb, recv_callback);
-  return ERR_OK;
-}
+const auto target = "255.255.255.255";
+const auto port = 3333;
 
 static void netif_status_callback(struct netif *netif) {
   if (netif_is_up(netif) && !ip4_addr_isany_val(*netif_ip4_addr(netif))) {
@@ -38,12 +18,13 @@ static void netif_status_callback(struct netif *netif) {
 int main() {
   stdio_init_all();
   sleep_ms(1000);
-  printf("Woke!\n");
+  printf("Initializing Wi-Fi...\n");
 
   if (cyw43_arch_init()) {
-    printf("failed to initialise\n");
+    printf("Failed to initialise!\n");
     return 1;
   }
+
   cyw43_arch_enable_sta_mode();
 
   struct netif *netif = &cyw43_state.netif[CYW43_ITF_STA];
@@ -52,22 +33,34 @@ int main() {
   printf("Connecting to Wi-Fi...\n");
   if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASS,
                                          CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-    printf("failed to connect.\n");
+    printf("Failed to connect!\n");
     return 1;
   } else {
     printf("Connected.\n");
   }
 
-  struct tcp_pcb *pcb = tcp_new();
-  tcp_bind(pcb, IP_ADDR_ANY, PORT);
-  pcb = tcp_listen(pcb);
-  tcp_accept(pcb, accept_callback);
+  picoosc::OSCClient client(target, port);
 
-  printf("TCP echo server started on port %d\n", PORT);
+  // Make an integer message
+  picoosc::OSCMessage msg;
 
   while (true) {
-    cyw43_arch_poll();
-  }
 
+    // cyw43_arch_lwip_begin();
+    msg.addAddress("/cont_1");
+    msg.add<float>(0.01);
+    msg.send(client);
+    msg.clear();
+
+    msg.addAddress("/cont_2");
+    msg.add<float>(0.7);
+    msg.send(client);
+    msg.clear();
+
+    // cyw43_arch_lwip_end();
+    // cyw43_arch_poll();
+    sleep_ms(500);
+  }
+  cyw43_arch_deinit();
   return 0;
 }
