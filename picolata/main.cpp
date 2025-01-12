66b0508ca83d6ae974e9fdf2eb212a24aeaf6e06
kureta <skureta@gmail.com>
2025-01-12 21:45:46 +0300

    initial working midi

--- picolata/main.cpp
+++ picolata/main.cpp
@@ -1,4 +1,5 @@
 #include "adc.hpp"
+#include "device/usbd.h"
 #include "modernosc.hpp"
 #include "mpu6050.hpp"
 #include "network.hpp"
@@ -12,10 +13,66 @@
 #include <string>
 #include <vector>
 
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+#include "bsp/board.h"
+#include "tusb.h"
+
+// Variable that holds the current position in the sequence.
+uint32_t note_pos = 0;
+
+// Store example melody as an array of note values
+uint8_t note_sequence[] = {
+    74, 78, 81, 86,  90, 93, 98, 102, 57, 61,  66, 69, 73, 78, 81, 85,
+    88, 92, 97, 100, 97, 92, 88, 85,  81, 78,  74, 69, 66, 62, 57, 62,
+    66, 69, 74, 78,  81, 86, 90, 93,  97, 102, 97, 93, 90, 85, 81, 78,
+    73, 68, 64, 61,  56, 61, 64, 68,  74, 78,  81, 86, 90, 93, 98, 102};
+
+void midi_task(void) {
+  static uint32_t start_ms = 0;
+  uint8_t msg[3];
+
+  // send note every 1000 ms
+  if (board_millis() - start_ms < 286)
+    return; // not enough time
+  start_ms += 286;
+
+  // Previous positions in the note sequence.
+  int previous = note_pos - 1;
+
+  // If we currently are at position 0, set the
+  // previous position to the last note in the sequence.
+  if (previous < 0)
+    previous = sizeof(note_sequence) - 1;
+
+  // Send Note On for current position at full velocity (127) on channel 1.
+  msg[0] = 0x90;                    // Note On - Channel 1
+  msg[1] = note_sequence[note_pos]; // Note Number
+  msg[2] = 127;                     // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Send Note Off for previous note.
+  msg[0] = 0x80;                    // Note Off - Channel 1
+  msg[1] = note_sequence[previous]; // Note Number
+  msg[2] = 0;                       // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Increment position
+  note_pos++;
+
+  // If we are at the end of the sequence, start over.
+  if (note_pos >= sizeof(note_sequence))
+    note_pos = 0;
+}
+
 #define SYSTEM_WAIT 1000
 #define BUFFER_SIZE 1024
 
 int main() {
+  board_init();
+
+  tusb_init();
   // Initialize stdio for printf
   stdio_init_all();
   // Have to wait a bit to be able to use printf
@@ -80,6 +137,9 @@ int main() {
     Buffer.clear();
     Buffer = OscBuilder::makeOscPacket("/temp", temp / 340.0F + 36.53F);
     Network::send(Buffer.data(), DestAddr, Port, Buffer.size());
+
+    tud_task();
+    midi_task();
 
     // Calculate elapsed time
     uint32_t ElapsedTime = to_ms_since_boot(get_absolute_time()) - StartTime;
